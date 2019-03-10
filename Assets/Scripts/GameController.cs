aa7e6c3e5054ec4d8631222d28121f31f0719593
Tao Guo <taoguo@cmail.carleton.ca>
2019-03-10 19:38:52 -0400

    drag selection
    drag

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Unit type enums
+    public enum UnitType {Land, Flying};
+    public enum User {Player, Computer};
+
+    private GameObject units;
+    //TEMP: Check that objects are being made right (ArrayList more efficient)
+    private GameObject playerUnits;
+    private GameObject computerUnits;
+
+    //TEMP: Colours for player and computer units
+    public Material playerFlying;
+    public Material playerLand;
+    public Material computerFlying;
+    public Material computerLand;
+
+    //TEMP: Need better way to load prefabs
+    public Rigidbody landVehicle;
+    public Rigidbody flyingVehicle;
+    //Load actual unit models below
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TEMP: Remove Testing Cube
+        this.transform.Find("Testing Cube").GetComponent<Renderer>().enabled = false;
+
+        //Setup Unit Storage GameObjects
+        units = new GameObject("Units");
+        playerUnits = new GameObject("Player Units");
+        playerUnits.transform.SetParent(units.transform);
+        computerUnits = new GameObject("Computer Units");
+        computerUnits.transform.SetParent(units.transform);
+
+        //Create Some initial units
+        CreateUnit(UnitType.Land, User.Player, new Vector3(0, 1, 0));
+        CreateUnit(UnitType.Land, User.Computer, new Vector3(0, 1, 7));
+        CreateUnit(UnitType.Flying, User.Player, new Vector3(0, 7.5f, 0));
+        CreateUnit(UnitType.Flying, User.Computer, new Vector3(0, 7.5f, 7));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateUnit(UnitType type, User user, Vector3 position)
+    {
+        //need to require that the spawned object is a rigidbody
+        switch (type)
+        {
+            case UnitType.Flying:
+                //NOTE: Convert this section to generic method later
+                Rigidbody flyingObj = (Rigidbody)Instantiate(flyingVehicle, position, flyingVehicle.transform.rotation);
+                flyingObj.transform.SetParent(units.transform);
+                if (user == User.Player)
+                {
+                    //playerUnits.Add(flyingObj);
+                    flyingObj.transform.SetParent(playerUnits.transform);
+                    flyingObj.GetComponent<Renderer>().material = playerFlying;
+                    flyingObj.gameObject.tag = "PlayerUnit";
+                }
+                else if (user == User.Computer)
+                {
+                    //computerUnits.Add(flyingObj);
+                    flyingObj.transform.SetParent(computerUnits.transform);
+                    flyingObj.GetComponent<Renderer>().material = computerFlying;
+                    flyingObj.gameObject.tag = "CompUnit";
+                }
+                //End Note
+                return;
+            case UnitType.Land:
+                Rigidbody landObj = (Rigidbody)Instantiate(landVehicle, position, Quaternion.identity);
+                landObj.transform.SetParent(units.transform);
+                if (user == User.Player)
+                {
+                    //playerUnits.Add(landObj);
+                    landObj.transform.SetParent(playerUnits.transform);
+                    landObj.GetComponent<Renderer>().material = playerFlying;
+                    landObj.gameObject.tag = "PlayerUnit";
+                }
+                else if (user == User.Computer)
+                {
+                    //computerUnits.Add(landObj);
+                    landObj.transform.SetParent(computerUnits.transform);
+                    landObj.GetComponent<Renderer>().material = computerFlying;
+                    landObj.gameObject.tag = "CompUnit";
+                }
+                return;
+            default:
+                Debug.Log("Error creating a unit");
+                return;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    List<ActionController> selectedUnits = new List<ActionController>();
+    RaycastHit hit;
+    bool isDragging = false;
+    Vector3 mousePosition;
+
+    private void OnGUI()
+    {
+        if (isDragging)
+        {
+            var rect = ScreenHelper.GetScreenRect(mousePosition, Input.mousePosition);
+            ScreenHelper.DrawScreenRect(rect,new Color(0.8f,0.8f,0.95f,0.1f));
+            ScreenHelper.DrawScreenRectBorder(rect, 1, Color.blue);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Input.mousePosition;
+            //Create a ray from the camera to the space
+            var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(camRay,out hit))
+            {
+                Debug.Log(hit.transform.tag);
+                if (hit.transform.CompareTag("PlayerUnit"))
+                {
+                    SelectUnit(hit.transform.gameObject.GetComponent<ActionController>(), Input.GetKey(KeyCode.LeftShift));
+                }
+                else
+                {
+                    isDragging = true;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            UnSelectUnits();
+            foreach(var selectableObject in FindObjectsOfType<BoxCollider>())
+            {
+                if (isWithinSelectionBound(selectableObject.transform)&&selectableObject.gameObject.tag=="PlayerUnit")
+                {
+                    SelectUnit(selectableObject.gameObject.GetComponent<ActionController>(), true);
+                }
+            }
+
+            isDragging = false;
+        }
+
+        if (Input.GetMouseButtonDown(1)&&selectedUnits.Count>0)
+        {
+            var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(camRay, out hit))
+            {
+                Debug.Log(hit.transform.tag);
+                if (hit.transform.CompareTag("Terrain"))
+                {
+                    foreach(var selectableObj in selectedUnits)
+                    {
+                        selectableObj.MoveUnit(hit.point);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SelectUnit(ActionController unit,bool isMultiSelect = false)
+    {
+        if (!isMultiSelect)
+        {
+            UnSelectUnits();
+        }
+        selectedUnits.Add(unit);
+        unit.SetSelected(true);
+
+    }
+
+    private void UnSelectUnits()
+    {
+        for(int i = 0; i < selectedUnits.Count; i++)
+        {
+            
+            selectedUnits[i].SetSelected(false);
+        }
+        selectedUnits.Clear();
+    }
+
+    private bool isWithinSelectionBound(Transform transform)
+    {
+        if (!isDragging)
+        {
+            return false;
+        }
+        var camera = Camera.main;
+        var viewportBounds = ScreenHelper.GetViewportBounds(camera,mousePosition,Input.mousePosition);
+        return viewportBounds.Contains(camera.WorldToViewportPoint(transform.position));
+    }
+}
