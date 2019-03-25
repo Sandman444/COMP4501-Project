9413e0fe46e9f1c44c0ac85c1d8cc57932821453
Tao Guo <taoguo@cmail.carleton.ca>
2019-03-24 21:53:45 -0400

    reworked version

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float scrollSpeed = 10.0f;
+    public float factor = 5.0f;
+    public Vector3 cameraAngle = new Vector3(45, 0, 0);
+    public float panBordertThickness = 2f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        factor = GetComponent<Camera>().fieldOfView / 60f * 10.0f;
+        if (Input.GetKey("up") || Input.mousePosition.y >= Screen.height - panBordertThickness)
+        {
+            pos.z += scrollSpeed * Time.deltaTime * factor;
+        }
+        if (Input.GetKey("down") || Input.mousePosition.y <= panBordertThickness)
+        {
+            pos.z -= scrollSpeed * Time.deltaTime * factor;
+        }
+        if (Input.GetKey("right") || Input.mousePosition.x >= Screen.width - panBordertThickness)
+        {
+            pos.x += scrollSpeed * Time.deltaTime * factor;
+        }
+        if (Input.GetKey("left") || Input.mousePosition.x <= panBordertThickness)
+        {
+            pos.x -= scrollSpeed * Time.deltaTime * factor;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            GetComponent<Camera>().fieldOfView--;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Camera>().fieldOfView++;
+        }
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Units/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    //Public Variables
+    public int startingHealth = 25;
+    public int currentHealth = 25;
+    public RectTransform healthBar;
+
+    bool damaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Damage Unit
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthBar.sizeDelta = new Vector2(100 * ((float)currentHealth/startingHealth), healthBar.sizeDelta.y);
+
+        //kill if health is zero
+        if (currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    //Kill the Unit if health reaches zero
+    void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
